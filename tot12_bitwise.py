c85a1ad4043f2f43c401b6392af32cf272dc0bf2
septianwijaya45 <septianwijaya45@gmail.com>
2023-07-31 19:27:15 +0700

    adding comment in tot12_bitwise

--- tot12_bitwise.py
+++ tot12_bitwise.py
@@ -1,3 +1,20 @@
+"""
+Operasi masing masing bit
+
+ex: 2 => 0 0 0 0 0 1   0 0
+         0 0 0 0 0 2^1 0 0 = 2
+
+ex: 1 => 0 0 0 0 0 0 0 0
+         0 0 0 0 0 0 0 2^0 = 1
+
+ex: 9 => 0 0 0 0 1   0 0 1
+         0 0 0 0 2^3 0 0 2^0 = 8 + 1 = 9
+
+         dijumlahkan ke kanan
+
+
+"""
+
 # operator bitwise, operasi biner, binary
 
 a = 9
@@ -30,6 +47,12 @@ print('nilai:',c, ', binary :', format(c, '08b'))
 
 
 # bitwise NOT (~) (mirror angka)
+"""
+mirroring angka pada 2^0 akan bergeser ke bit 1, dan menjadi minus
+ex: 9 => 00001001
+menjadi
+    -10 => -0001010
+"""
 c = ~a
 print('\n================= NOT ================')
 print('nilai:',a, ', binary :', format(a, '08b'))
@@ -42,7 +65,7 @@ print('nilai :', e^d, ', binary :', format(e^d, '08b'))
 # shifting (bergeser)
 
 # shift right (>>)
-c = a >> 2
+c = a >> 4
 print('\n================= >> ================')
 print('nilai:',a, ', binary :', format(a, '08b'))
 print('nilai:',c, ', binary :', format(c, '08b'))
